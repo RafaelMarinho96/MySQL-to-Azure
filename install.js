067570ffa43c2fec58f3e97765413d0ffbeb2480
Rafael Marinho <rafael.marinho07@gmail.com>
2020-01-29 11:51:07 -0300

    Add .env variables on uninstall.js and install.js

--- install.js
+++ install.js
@@ -1,9 +1,11 @@
 const Service = require('node-windows').Service;
 
+require('dotenv').config();
+
 let svc = new Service({
-    name: '<script-name>',
-    description: 'Upload mysql backups to azure blob',
-    script: '<script-path>'
+    name: process.env.SERVICE_NAME,
+    description: process.env.SERVICE_DESCRIPTION,
+    script: require('path').join(__dirname, process.env.SCRIPT_NAME)
 })
 
 svc.on('install', function() {
